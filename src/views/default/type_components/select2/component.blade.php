31770b541d2953f4afdab71bb3d387d9c88f9d59
Foubian <foubiantux@gmail.com>
2024-03-14 21:03:44 +0000

    Update component.blade.php

--- src/views/default/type_components/select2/component.blade.php
+++ src/views/default/type_components/select2/component.blade.php
@@ -118,7 +118,7 @@
 @endif
 
 <div class='form-group {{$header_group_class}} {{ ($errors->first($name))?"has-error":"" }}' id='form-group-{{$name}}' style="{{@$form['style']}}">
-    <label class='control-label col-sm-2'>{{$form['label']}}
+    <label class='control-label col-sm-2'>{{$form['label']}}aa
         @if($required)
             <span class='text-danger' title='{!! cbLang('this_field_is_required') !!}'>*</span>
         @endif
