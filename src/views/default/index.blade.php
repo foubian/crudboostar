1505aa9d359a9371e0aa6b066a6105c717de52df
Foubian <fouad.elrharbi@gmail.com>
2024-03-18 13:02:45 +0000

    Bulck completed

--- src/views/default/index.blade.php
+++ src/views/default/index.blade.php
@@ -191,7 +191,7 @@
 
                             <div class="hex" style="width: 34.64px;height: 40px;background:DarkGray;">
                                 <div class="hex-background" style="width: 30.64px;height: 36px;  background: white;">
-                                    <div class="overlayhex">
+                                    <div class="overlayhex selected-action">
                                         <a href="javascript:void(0)" data-name='delete'
                                             title='{{ cbLang('action_delete_selected') }}'>
                                             <span class="spin-icon">
@@ -205,13 +205,6 @@
                             
                             @endif
 
-                            @if ($button_selected)
-                                @foreach ($button_selected as $button)
-                                    <a href="javascript:void(0)" data-name='{{ $button['name'] }}'
-                                            title='{{ $button['label'] }}'><i class="fa fa-{{ $button['icon'] }}"></i>
-                                            {{ $button['label'] }}</a>
-                                @endforeach
-                            @endif
 
                         <!--end-dropdown-menu-->
                     </div>
